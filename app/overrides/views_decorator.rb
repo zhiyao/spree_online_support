c0565c0d00139a2ac64fbc5b8c570631622b8189
Kenny Chan Zhi Yao <zhiyao.chan@gmail.com>
2013-11-06 02:31:40 +0800

    update view decorator for missing translation

--- app/overrides/views_decorator.rb
+++ app/overrides/views_decorator.rb
@@ -2,7 +2,7 @@ Deface::Override.new(
   :virtual_path => "spree/admin/shared/_configuration_menu",
   :name => "spree_online_support_admin_configurations_sidebar_menu",
   :insert_bottom => "[data-hook='admin_configurations_sidebar_menu'], #admin_configurations_sidebar_menu[data-hook]",
-  :text => "<%= configurations_sidebar_menu_item t(:online_support), admin_online_support_setting_path %>",
+  :text => "<%= configurations_sidebar_menu_item Spree.t(:online_support), admin_online_support_setting_path %>",
   :disabled => false)
 
 Deface::Override.new(
